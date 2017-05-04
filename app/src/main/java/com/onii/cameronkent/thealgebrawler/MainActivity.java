62772169727c5ac5a1834882613387e31b562511
cameronkent <cameron.kent@my.jcu.edu.au>
2017-05-04 15:09:07 +1000

    added backgrounds to landscape layouts

--- app/src/main/java/com/onii/cameronkent/thealgebrawler/MainActivity.java
+++ app/src/main/java/com/onii/cameronkent/thealgebrawler/MainActivity.java
@@ -1,6 +1,7 @@
 package com.onii.cameronkent.thealgebrawler;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO: 20/04/2017 setup actionBar
 
+    private SharedPreferences SETTINGS;
     private Button playGameButton, highScoresButton;
 
     @Override
@@ -20,8 +22,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        SETTINGS = getSharedPreferences("SETTINGS", MODE_PRIVATE);
         playGameButton = (Button) findViewById(R.id.play_game_button);
         highScoresButton = (Button) findViewById(R.id.high_scores_button);
+
+//        styleButtons();
 
         /***/
         playGameButton.setOnClickListener(new View.OnClickListener() {
@@ -50,4 +55,31 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+
+//    private void styleButtons() {
+//        String backgroundChoice = SETTINGS.getString("backgroundChoice", "Green");
+//        switch (backgroundChoice) {
+//            case "Green":
+//                playGameButton.setBackgroundResource(R.drawable.bg_green_button);
+//                highScoresButton.setBackgroundResource(R.drawable.bg_green_button);
+//                break;
+//            case "Desert":
+//                playGameButton.setBackgroundResource(R.drawable.bg_desert_button);
+//                highScoresButton.setBackgroundResource(R.drawable.bg_desert_button);
+//                break;
+//            case "Winter":
+//                playGameButton.setBackgroundResource(R.drawable.bg_winter_button);
+//                highScoresButton.setBackgroundResource(R.drawable.bg_winter_button);
+//                break;
+//            case "Graveyard":
+//                playGameButton.setBackgroundResource(R.drawable.bg_graveyard_button);
+//                highScoresButton.setBackgroundResource(R.drawable.bg_graveyard_button);
+//                break;
+//            case "Sci-Fi":
+//                playGameButton.setBackgroundResource(R.drawable.bg_scifi_button);
+//                highScoresButton.setBackgroundResource(R.drawable.bg_scifi_button);
+//                break;
+//        }
+//
+//    }
 }
